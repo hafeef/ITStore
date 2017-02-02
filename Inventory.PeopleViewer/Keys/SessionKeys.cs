6ad255f1dd1eef9fc1ef1a02c52c7aae14400146
SHAIK HAFEEF <shaik.hafeef@thomsonreuters.com>
2017-02-02 17:53:17 +0530

    Added SessionKeys File

---
+++ Inventory.PeopleViewer/Keys/SessionKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.PeopleViewer.Keys
+{
+    public static class SessionKeys
+    {
+        public const string UserID = "UserID";
+        public const string UserName = "UserName";
+    }
+}
